6b070a0f2bf0198dde0af13cc068280bd6739052
Shridhar Gaikwad <shridhargaikwad333@gmail.com>
2026-08-05 01:32:38 +0530

    "Record example."

--- src/main/java/com/preperation/Main.java
+++ src/main/java/com/preperation/Main.java
@@ -1,19 +1,15 @@
 package com.preperation;
 
+import com.preperation.java.Java17.RecordExample;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
-        String str = "Java";
-        StringBuilder sb = new StringBuilder();
-        int times = 3;
-        String updatedString = str.repeat(3);
-        System.out.println(updatedString);
-        for(int i=0; i<times; i++){
-            sb.append(str).append("\n");
-        }
 
-//        System.out.println(sb.toString());
+    public static void main(String[] args){
+
+        RecordExample recordExample = new RecordExample(1, "John Doe", "Developer", 75000.0);
+        System.out.println("Record Example: " + recordExample);
 
     }
 }
---
+++ src/main/java/com/preperation/java/Java17/RecordExample.java
@@ -0,0 +1,6 @@
+package com.preperation.java.Java17;
+
+public record RecordExample(int id, String name, String role, double salary) {
+    // The record automatically generates the constructor, getters, equals(), hashCode(), and toString() methods.
+
+}
