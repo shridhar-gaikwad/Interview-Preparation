2ecb818e72d186e257ef69cd2780bdd8dd36819e
Shridhar Gaikwad <shridhargaikwad333@gmail.com>
2026-08-13 21:59:41 +0530

    "Immutable class & mutable object related changes."

--- src/main/java/com/preperation/java/FinalClassExample.java
+++ src/main/java/com/preperation/java/FinalClassExample.java
@@ -1,9 +1,18 @@
 package com.preperation.java;
 
-public final class FinalClassExample {
+import java.util.Date;
 
-    private FinalClassExample() {
+public final class FinalClassExample {          // final class cannot be subclassed
+
+    private final String name;                  // private final field
+    private final int age;
+    private final Date birthDate;
+
+    private FinalClassExample(String name, int age, Date birthDate) {           // initializing final fields in constructor
         // private constructor to prevent instantiation
+        this.name = name;
+        this.age = age;
+        this.birthDate = new Date(birthDate.getTime());                         // defensive copy
     }
 
 }
