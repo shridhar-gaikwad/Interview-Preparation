b4220037e997ea071499ab1eb891f7ca34c9fa4a
Shridhar Gaikwad <shridhargaikwad333@gmail.com>
2026-08-17 22:01:17 +0530

    "Main class changes."

--- src/main/java/com/preperation/Main.java
+++ src/main/java/com/preperation/Main.java
@@ -2,7 +2,7 @@ package com.preperation;
 
 import com.preperation.java.Java17.RecordExample;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
@@ -11,5 +11,9 @@ public class Main {
         RecordExample recordExample = new RecordExample(1, "John Doe", "Developer", 75000.0);
         System.out.println("Record Example: " + recordExample);
 
+        int a = 100;
+        long b = 1000;
+
+        long c = a + b;
     }
 }
