5371c95b7b18f2e12d6a8396dd25ccea0754fc8a
Shridhar.Gaikwad <shridhargaikwad333@gmail.com>
2026-08-18 14:23:07 +0530

    Change in EmployeeRecord.

---
+++ src/main/java/com/preperation/java/Java17/EmployeeRecord.java
@@ -0,0 +1,33 @@
+package com.preperation.java.Java17;
+
+/*
+Java automatically generates:
+
+Private final fields
+Constructor
+Getter-like methods (id(), name())
+equals()
+hashCode()
+toString()
+ */
+public record EmployeeRecord(int id, String name, String role, double salary) {
+
+//    Compact Constructor (Can add validation logic)
+    public EmployeeRecord {
+        if(id < 0 || role == null || name == null || salary < 0){
+            throw new IllegalArgumentException("Invalid Input.");
+        }
+    }
+
+    public String displayIDName(){
+        return id + " " + name;
+    }
+}
+
+/*
+Features of Records
+1. Immutable by Default
+2. Automatic Methods (equals(), hashCode(), toString())
+3. Compact Constructor
+4. Additional Methods Allowed
+*/
--- src/main/java/com/preperation/java/Java17/RecordExample.java
+++
@@ -1,6 +0,0 @@
-package com.preperation.java.Java17;
-
-public record RecordExample(int id, String name, String role, double salary) {
-    // The record automatically generates the constructor, getters, equals(), hashCode(), and toString() methods.
-
-}
