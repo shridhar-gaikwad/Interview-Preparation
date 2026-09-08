a91c665cd802404404e294c3571bdc204916c0b0
Shridhar.Gaikwad <shridhargaikwad333@gmail.com>
2026-09-08 16:20:07 +0530

    InterviewQuestions and Checklist added.

---
+++ src/main/java/com/preperation/interviewQue/InterviewChecklist.java
@@ -0,0 +1,23 @@
+package com.preperation.interviewQue;
+
+public interface InterviewChecklist {
+
+    /*
+    Microservices Architecture Patterns
+        API Gateway
+        Saga Pattern
+
+    Design Patterns
+        Creational Design Patterns
+        Structural Design Patterns
+        Behavioral Design Patterns
+
+    Solid Principles
+
+    Scenario Based Questions
+
+    Collection Frameworks
+
+    Microser
+    */
+}
